78daf0312c57f87ecc5f33269d735410c0bf3546
Lisa <lalisssa.monobal@gmail.com>
2026-03-10 17:35:44 +0200

    made wth-btn selectable

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,15 +1,35 @@
+import { useState } from 'react'
 import '../App.css'
 
 export default function Weather() {
+  const [selectedWth, setSelectedWth] =useState(null);
+
+  const weathers = [
+    {emoji: '☀️', value: 'sunny', id: 'sunny-btn'},
+    {emoji: '☁️', value: 'cloudy'},
+    {emoji: '🌧️', value: 'rainy'},
+    {emoji: '❄️', value: 'snowy'},
+    {emoji: '🌬️', value: 'windy'},
+  ];
+
 return(
 <div className="weather-container">
         <label htmlFor="weather" className="weather-qn">How was the weather today?🍂</label>
         <div id="weather" className="weather-btns">
-          <button id="sun-btn"className="wth-emoji">☀️</button>
-          <button className="wth-emoji">☁️</button>
-          <button className="wth-emoji">🌧️</button>
-          <button className="wth-emoji">❄️</button>
-          <button className="wth-emoji">🌬️</button>
+          {weathers.map((w) =>(
+            <button
+            key={w.value}
+            id={w.id}
+            className={`wth-emoji ${selectedWth === w.value ? 'selected-wth' : ''}`}
+            onClick={() => {
+              setSelectedWth(
+                selectedWth === w.value ? null : w.value);
+              }}
+            >
+              {w.emoji}
+          </button>
+          )
+        )}
         </div>
       </div>
     )
